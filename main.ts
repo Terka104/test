006d293fb0a8c67da60bfe0a9b972dfa8c2dcf64
Terka104 <146720298+Terka104@users.noreply.github.com>
2024-03-08 11:10:04 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+type Hodnoceni = {
+    jmeno: string,
+    znamka: number
+}
+// Máte seznam jmen a známek studentů ve třídě. 
+// Například: 
+let znamky: Array<Hodnoceni> = [
+    { jmeno: "Petr", znamka: 2 },
+    { jmeno: "Anna", znamka: 1 },
+    { jmeno: "Jana", znamka: 3 },
+    { jmeno: "David", znamka: 1 }
+]
+
+function Seznam(zmamky:Array; ) {
+    for
+}
+/* 
+Napište algoritmus, který vytvoří nový seznam 
+obsahující pouze jména studentů s nejlepší známkou. 
+Pokud je více studentů se stejnou nejlepší známkou, 
+zahrňte je všechny do seznamu.
+*/
+const novySeznam: Array<string> = []; //Například: ["Anna", "David"].
+
+console.logValue("šprti", novySeznam)
